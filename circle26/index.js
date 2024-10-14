88e158b32ace6652ee3f66242ca36f7a9674be35
Bok Victoria <54582077+VickyBok6@users.noreply.github.com>
2024-10-14 20:58:35 +0100

    Add files via upload

---
+++ circle26/index.js
@@ -0,0 +1,20 @@
+// 1. How do you write a compound word in JavaScript?
+const fruitSalad = 'orange_banana'
+console.log(' The compound word is ' + `${fruitSalad}`);
+
+// 2. Ask: Ask the user to fill a prompt box with their name which returns Hello [userName]
+let fullName = prompt('What is your full name?');
+console.log(" Hello, " + `${fullName}`);
+
+// 3. Write a ifelse statement that check if 30 is greater 10, if yes, log true, if not, log false.
+let a = 30;
+if( a>10){
+    console.log('True')
+}else{
+    console.log('False')
+}
+
+
+//4. Given a string called JavaScript, how will check for it's length?
+const lan = "JavaScript";
+console.log(lan.length);
